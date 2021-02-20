3f61e329671db647ae40e6254ea9158cd3e4d912
Brian Hart <brian.hart@xylogix.net>
2021-02-19 19:47:16 -0500

    Create xyLOGIX.Api.Data.Providers.Events.csproj

---
+++ xyLOGIX.Api.Data.Providers.Events/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Support Library for xyLOGIX.Api.Data.Providers.Events")]
+[assembly: AssemblyDescription("Provides objects and functionality for ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for xyLOGIX.Api.Data.Providers.Events")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2020 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX xyLOGIX.Api.Data.Providers.  xyLOGIX.Api.Data.Providers is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1ce2181-07bb-4996-b706-d9f448c3ec65")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
